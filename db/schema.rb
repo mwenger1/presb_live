e697b3734f6a9af338ad7e3e3e50af110584ccc3
Michael Wenger <michaelwenger27@gmail.com>
2014-03-15 19:00:26 -0400

    Updated tag and seeds files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140315223857) do
+ActiveRecord::Schema.define(version: 20140315225145) do
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
