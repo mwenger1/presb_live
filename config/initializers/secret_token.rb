2e887e67700508865bd63927c6a7fc70a8b65e68
Michael Wenger <michaelwenger27@gmail.com>
2014-03-15 17:48:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PresbLive::Application.config.secret_key_base = 'b7dd1b672ff412bdc797fed94ad75a55d2e73fea2fa161dffc1129197ee06dc1cd2c9e764411b2b54b4b6bbe7068f9f08ac4a39269944d16d0f94b9541afe2b1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PresbLive::Application.config.session_store :cookie_store, key: '_Presb_live_session'
