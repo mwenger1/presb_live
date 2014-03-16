9aa9ae30a7df9451a1d088bd70f2ad4b5d48b76a
opihana <hana378@gmail.com>
2014-03-15 22:56:32 -0400

    Additional css and route file changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,11 +28,9 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
+        format.html { redirect_to hangouts_path, notice: 'User was successfully created.' }
       else
         format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
